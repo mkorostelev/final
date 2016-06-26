385d556be839deb21630b53dcb336a5376953c18
Maks Korostelov <kostul89@gmail.com>
2016-06-26 19:24:33 +0300

    Routes works again

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,11 +2,11 @@ class Route < ApplicationRecord
   has_many :route_points, dependent: :destroy
   has_many :business_processes
   has_many :tasks #, through: :route_points
-  belongs_to :user
-  
+  # belongs_to :user
+
   # accepts_nested_attributes_for :route_points, allow_destroy: true
   #accepts_nested_attributes_for :route_points, reject_if: lambda {|attributes| attributes['title'].blank?}
-  
+
   validates :title, :description, :author_id, presence: true
   validates :title, uniqueness: true
   validates :title, length: { minimum: 3 }
